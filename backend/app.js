89e6a2534873c313b8d8ce893a94204869812415
soheb21 <neoshoeb53@gmail.com>
2024-07-25 18:36:12 +0530

    Creating build for deployment

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,8 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static("./dist"))
+
 
 //routes
 app.use("/api/v1/user", require("./routes/userRoute"))
