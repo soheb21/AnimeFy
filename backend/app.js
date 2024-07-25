a2e35b358fbcec4d29c85bc371ddae4999017f33
soheb21 <neoshoeb53@gmail.com>
2024-07-25 18:51:42 +0530

    Attaching build file to backend

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,14 @@ const app = express();
 const cors = require("cors");
 const { connectDB } = require("./Db/connectDB");
 const isAuthenticate = require("./middleware/isAuth");
+const path = require("path")
 
 //middleware
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("./dist"))
+app.use(express.static(path.resolve(__dirname, 'dist')))
 
 
 //routes
